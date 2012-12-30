1885a92d95925c33565f8d841411b441fe3b81b7
Siddharth Ram <siddharthram@gmail.com>
2012-12-29 16:33:19 -0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Methamagic::Application.config.secret_token = 'c89e52f84fb91a0104f6a3c9ebd26fc648590f6da1e2e03cb834f68ed5d7761583e0f8b73d05314088801f7ae5d97274f3d9c20fe43ba41770406056f6ee2abb'
